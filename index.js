37452432b57793054e2548676518cdd0afb707a1
Avant Baker <avantbaker@Avants-MacBook-Air.local>
2023-08-03 13:49:11 -0500

    Updated Auth to use the Token

--- index.js
+++ index.js
@@ -4,104 +4,90 @@ const http = require("http");
 const httpServer = http.Server;
 
 class ContentfulWebhookListener extends httpServer {
-    constructor (opts, requestListener) {
+  constructor(opts, requestListener) {
+    super(requestListener);
 
-        super(requestListener);
+    let server = this;
 
-        let server = this;
+    this.on("request", function (request, response) {
+      let auth,
+        authorization,
+        body,
+        contentType,
+        event,
+        kind,
+        origin,
+        topic,
+        webhookName;
 
-        this.on("request", function (request, response) {
+      auth = opts.auth || null;
 
-            let auth, authInBase64, authorization, body, contentType, event, kind, origin, topic, webhookName;
+      // verify content-type
+      contentType = request.headers["content-type"];
+      if (contentType !== "application/vnd.contentful.management.v1+json") {
+        response.writeHead(406, "Not Acceptable");
+        response.end();
+        return;
+      }
 
-            auth = opts.auth || null;
+      // verify authorization
+      if (auth) {
+        authorization = request.headers["authorization"];
+        if (authorization !== `Basic ${auth}`) {
+          response.writeHead(401, "Unauthorized");
+          response.end();
+          return;
+        }
+      }
 
-            // verify content-type
-            contentType = request.headers["content-type"];
-            if (contentType !== "application/vnd.contentful.management.v1+json") {
+      // capture custom headers from Contetnful
+      topic = request.headers["x-contentful-topic"];
+      [origin, kind, event] = topic.split(".");
+      webhookName = request.headers["x-contentful-webhook-name"];
 
-                response.writeHead(406, "Not Acceptable");
-                response.end();
-                return;
+      // camelcase event name because that is the convention in node
+      event = event.replace(/_([a-z])/, function (match, p1) {
+        return p1.toUpperCase();
+      });
 
-            }
+      // capture body
+      body = "";
+      request.on("data", function (chunk) {
+        body += chunk.toString();
+      });
 
-            // verify authorization
-            if (auth) {
+      // parse body
+      request.on("end", function () {
+        response.writeHead(200, "OK");
 
-                authorization = request.headers["authorization"];
-                authInBase64 = new Buffer(auth).toString("base64");
-                if (authorization !== `Basic ${authInBase64}`) {
+        try {
+          body = JSON.parse(body);
 
-                    response.writeHead(401, "Unauthorized");
-                    response.end();
-                    return;
+          // emit event with webhook object
+          let webhook = {
+            contentType: body.sys.contentType && body.sys.contentType.sys.id,
+            fields: body.fields,
+            id: body.sys.id,
+            kind: kind,
+            origin: origin,
+            space: body.sys.space.sys.id,
+            sys: body.sys,
+            webhookName: webhookName,
+          };
+          server.emit(event, webhook);
+        } catch (err) {
+          server.emit("error", err);
+          server.close();
+        }
 
-                }
-
-            }
-
-            // capture custom headers from Contetnful
-            topic = request.headers["x-contentful-topic"];
-            [origin, kind, event] = topic.split(".");
-            webhookName = request.headers["x-contentful-webhook-name"];
-
-            // camelcase event name because that is the convention in node
-            event = event.replace(/_([a-z])/, function (match, p1) {
-
-                return p1.toUpperCase();
-
-            });
-
-            // capture body
-            body = "";
-            request.on("data", function (chunk) {
-
-                body += chunk.toString();
-
-            });
-
-            // parse body
-            request.on("end", function () {
-
-                response.writeHead(200, "OK");
-
-                try {
-
-                    body = JSON.parse(body);
-
-                    // emit event with webhook object
-                    let webhook = {
-                        "contentType": body.sys.contentType && body.sys.contentType.sys.id,
-                        "fields": body.fields,
-                        "id": body.sys.id,
-                        "kind": kind,
-                        "origin": origin,
-                        "space": body.sys.space.sys.id,
-                        "sys": body.sys,
-                        "webhookName": webhookName
-                    };
-                    server.emit(event, webhook);
-
-                } catch (err) {
-
-                    server.emit("error", err);
-                    server.close();
-
-                }
-
-                response.end();
-
-            });
-
-        });
-    }
+        response.end();
+      });
+    });
+  }
 }
 
 exports.Server = ContentfulWebhookListener;
 
-exports.createServer = function(opts, requestListener) {
-
-    return new ContentfulWebhookListener(opts, requestListener);
-
+exports.createServer = function (opts, requestListener) {
+  return new ContentfulWebhookListener(opts, requestListener);
 };
